4844e7d53a6099389ac74e7f25ff829c490a4923
Vihar <202001110@daiict.ac.in>
2022-04-12 16:07:09 +0530

    File renamed and reformatted

--- encoder.py
+++ encoder.py
@@ -1,12 +1,13 @@
-from base import MORSE_CODE_DICT
+from DataSet import MORSE_CODE_DICT
 
-def encode(message : str | None):
+
+def encode(message: str | None):
     message = message.strip().upper()
     if message is None or message == "":
         return None
-    
+
     encoded_string = ""
     for letter in message:
-        encode = MORSE_CODE_DICT.get( letter, letter ) + " "
+        encode = MORSE_CODE_DICT.get(key=letter, default=letter) + " "
         encoded_string += encode
     return encoded_string
