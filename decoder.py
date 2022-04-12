e78949423325b3f7c23dfd4f74b88e4e6a3f6c58
Vihar <202001110@daiict.ac.in>
2022-04-12 16:06:51 +0530

    File renamed and reformatted

--- decoder.py
+++ decoder.py
@@ -1,28 +1,28 @@
-from base import MORSE_CODE_DICT
-import re
-import encoder
+from DataSet import MORSE_CODE_DICT
 
-def decode(encoded_string : str | None):
+
+def decode(encoded_string: str | None):
     encoded_string = encoded_string.strip() + " "
 
     if encoded_string is None or encoded_string == "":
         return None
 
     message = ""
-    encode = ""
+    encoding = ""
 
     for letter in encoded_string:
         if letter == '.' or letter == '-':
-            encode += letter
+            encoding += letter
             i = 0
-        elif  letter == ' ':
+        elif letter == ' ':
             i += 1
             if i == 1:
-                message += list(MORSE_CODE_DICT.keys())[ list(MORSE_CODE_DICT.values()).index(encode) ]
-                encode = ""
+                message += list(MORSE_CODE_DICT.keys()
+                                )[list(MORSE_CODE_DICT.values()).index(encoding)]
+                encoding = ""
             elif i == 2:
                 message += " "
         else:
             message += letter
-    
+
     return message
